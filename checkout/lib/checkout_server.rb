cf5d4222cedb769cc0da137b819ecbb263e9e396
Marcos Brizeno <marcos.uece.comp@gmail.com>
2018-02-16 13:10:48 -0200

    Improve checkout service logs

--- checkout/lib/checkout_server.rb
+++ checkout/lib/checkout_server.rb
@@ -3,7 +3,7 @@ require 'protos/checkout_services_pb'
 class CheckoutServer < Store::Checkout::Service
 
   def process_payment(checkout_request, _unused_call)
-    puts checkout_request
+    puts build_request_log(checkout_request)
     discount_amount = 100
     paid_amount = checkout_request.cart_total - discount_amount
     Store::CheckoutResponse.new(
@@ -12,6 +12,15 @@ class CheckoutServer < Store::Checkout::Service
       paid_amount: paid_amount,
       status: Store::CheckoutStatus::SUCCESSFUL
     )
+  end
+
+  private
+  def build_request_log(checkout_request)
+    "-----------------------------
+    Received Checkout request
+    Products: #{checkout_request.product_id}
+    User uid: #{checkout_request.user_uid}
+    Cart total: #{checkout_request.cart_total}"
   end
 
 end
--- checkout/test_client.rb
+++ checkout/test_client.rb
@@ -13,7 +13,7 @@ checkout_request = Store::CheckoutRequest.new(
   cart_total: 1000)
 checkout_request.product_id += [1,2,3]
 checkout_response = stub.process_payment(checkout_request)
-puts """Checkout status: #{checkout_response.status}
+puts "Checkout status: #{checkout_response.status}
       Cart Total: #{checkout_response.cart_total}
       Discounts: #{checkout_response.discount_total}
-      Paid amount: #{checkout_response.paid_amount}"""
+      Paid amount: #{checkout_response.paid_amount}"
